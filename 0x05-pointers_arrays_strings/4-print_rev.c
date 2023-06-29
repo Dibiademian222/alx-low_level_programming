792f2c2e564ded02bb8d6d692d4c2b6312750994
Dibiademian222 <chikadibia3891@gmail.com>
2023-06-29 07:30:57 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to print
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (i = length; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+
